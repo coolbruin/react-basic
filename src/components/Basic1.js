029a07af47667b0213e996b1b09f59e4dc14655b
coolbruin <yoshio.sakuma.engineer@gmail.com>
2022-01-02 15:58:12 +0900

    useState(Object)

--- src/components/Basic1.js
+++ src/components/Basic1.js
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Basic1 = (props) => {
-  const clickHandler = () => {
-    console.log("clicked");
-  };
+const Basic1 = () => {
+  const [product, setProducts] = useState({ name: "", price: "" });
 
   return (
     <>
-      <button onClick={clickHandler}>Click</button>
-      <h1>Hello {props.name}</h1>
-      <h1>Hello React2</h1>
+      <form>
+        <input
+          type="text"
+          value={product.name}
+          onChange={(evt) =>
+            setProducts({ ...product, name: evt.target.value })
+          }
+        />
+
+        <input
+          type="text"
+          value={product.price}
+          onChange={(evt) =>
+            setProducts({ ...product, price: evt.target.value })
+          }
+        />
+      </form>
+      <h3>Product name is {product.name}</h3>
+      <h3>Product price is {product.price}</h3>
     </>
   );
 };
