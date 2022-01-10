08294ccf7a86811a1e30a1b7561208ac88447807
coolbruin <yoshio.sakuma.engineer@gmail.com>
2022-01-10 13:00:40 +0900

    useContext 実装

--- src/App.js
+++ src/App.js
@@ -3,19 +3,24 @@ import "./App.css";
 // import Basic2 from "./components/Basic2";
 // import BasicUseEffect from "./components/BasicUseEffect";
 // import TimerContainer from "./components/TimeContainer";
-import ApiFetch from "./components/ApiFetch";
+// import ApiFetch from "./components/ApiFetch";
+import AppContext from "./contexts/AppContext";
+import B from "./components/B";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {/* <Basic2 />
+    <AppContext.Provider value={"value from App.js"}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          {/* <Basic2 />
         <BasicUseEffect /> */}
-        {/* <TimerContainer /> */}
-        <ApiFetch />
-      </header>
-    </div>
+          {/* <TimerContainer /> */}
+          {/* <ApiFetch /> */}
+          <B />
+        </header>
+      </div>
+    </AppContext.Provider>
   );
 }
 
