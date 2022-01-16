11c4341ce137024f237370b42d92c7aaff9eb208
coolbruin <yoshio.sakuma.engineer@gmail.com>
2022-01-16 14:31:15 +0900

    useReducer + useContext組み合わせ

--- src/App.js
+++ src/App.js
@@ -6,10 +6,31 @@ import "./App.css";
 // import ApiFetch from "./components/ApiFetch";
 import AppContext from "./contexts/AppContext";
 import B from "./components/B";
+import BasicReducer from "./components/BasicReducer";
+import CompB from "./components/CompB";
+
+import { useReducer } from "react";
+
+const initialState = 0;
+const reducer = (currentState, action) => {
+  switch (action) {
+    case "add_1":
+      return currentState + 1;
+    case "multiple_3":
+      return currentState + 3;
+    case "reset":
+      return initialState;
+    default:
+      return currentState;
+  }
+};
 
 function App() {
+  const [count, dispatch] = useReducer(reducer, initialState);
   return (
-    <AppContext.Provider value={"value from App.js"}>
+    <AppContext.Provider
+      value={{ countProvided: count, dispatchProvided: dispatch }}
+    >
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -17,7 +38,10 @@ function App() {
         <BasicUseEffect /> */}
           {/* <TimerContainer /> */}
           {/* <ApiFetch /> */}
-          <B />
+          {/* <B /> */}
+          {/* <BasicReducer /> */}
+          Count {count}
+          <CompB />
         </header>
       </div>
     </AppContext.Provider>
